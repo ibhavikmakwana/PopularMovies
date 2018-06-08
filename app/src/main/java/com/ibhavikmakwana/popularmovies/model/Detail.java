831b4fa41c79d9c7684c2ce5f5f916bc720b763d
Bhavik Makwana <bhavik.makwana@multidots.com>
2018-06-08 18:50:13 +0530

    Detail pojo

--- app/src/main/java/com/ibhavikmakwana/popularmovies/model/Detail.java
+++ app/src/main/java/com/ibhavikmakwana/popularmovies/model/Detail.java
@@ -1,7 +1,255 @@
 package com.ibhavikmakwana.popularmovies.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * Created by Bhavik Makwana on 08-06-2018.
  */
 public class Detail {
+    @SerializedName("adult")
+    @Expose
+    private Boolean adult;
+    @SerializedName("backdrop_path")
+    @Expose
+    private String backdropPath;
+    @SerializedName("belongs_to_collection")
+    @Expose
+    private BelongsToCollection belongsToCollection;
+    @SerializedName("budget")
+    @Expose
+    private Integer budget;
+    @SerializedName("genres")
+    @Expose
+    private List<Genre> genres = null;
+    @SerializedName("homepage")
+    @Expose
+    private String homepage;
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("imdb_id")
+    @Expose
+    private String imdbId;
+    @SerializedName("original_language")
+    @Expose
+    private String originalLanguage;
+    @SerializedName("original_title")
+    @Expose
+    private String originalTitle;
+    @SerializedName("overview")
+    @Expose
+    private String overview;
+    @SerializedName("popularity")
+    @Expose
+    private Double popularity;
+    @SerializedName("poster_path")
+    @Expose
+    private String posterPath;
+    @SerializedName("production_companies")
+    @Expose
+    private List<ProductionCompany> productionCompanies = null;
+    @SerializedName("release_date")
+    @Expose
+    private String releaseDate;
+    @SerializedName("revenue")
+    @Expose
+    private Integer revenue;
+    @SerializedName("runtime")
+    @Expose
+    private Integer runtime;
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("tagline")
+    @Expose
+    private String tagline;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("video")
+    @Expose
+    private Boolean video;
+    @SerializedName("vote_average")
+    @Expose
+    private Double voteAverage;
+    @SerializedName("vote_count")
+    @Expose
+    private Integer voteCount;
+
+    public Boolean getAdult() {
+        return adult;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public BelongsToCollection getBelongsToCollection() {
+        return belongsToCollection;
+    }
+
+    public Integer getBudget() {
+        return budget;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public Double getPopularity() {
+        return popularity;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public List<ProductionCompany> getProductionCompanies() {
+        return productionCompanies;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public Integer getRevenue() {
+        return revenue;
+    }
+
+    public Integer getRuntime() {
+        return runtime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getTagline() {
+        return tagline;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Boolean getVideo() {
+        return video;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    public class BelongsToCollection {
+
+        @SerializedName("id")
+        @Expose
+        private Integer id;
+        @SerializedName("name")
+        @Expose
+        private String name;
+        @SerializedName("poster_path")
+        @Expose
+        private String posterPath;
+        @SerializedName("backdrop_path")
+        @Expose
+        private String backdropPath;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPosterPath() {
+            return posterPath;
+        }
+
+        public String getBackdropPath() {
+            return backdropPath;
+        }
+    }
+
+    public class Genre {
+
+        @SerializedName("id")
+        @Expose
+        private Integer id;
+        @SerializedName("name")
+        @Expose
+        private String name;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public class ProductionCompany {
+
+        @SerializedName("id")
+        @Expose
+        private Integer id;
+        @SerializedName("logo_path")
+        @Expose
+        private String logoPath;
+        @SerializedName("name")
+        @Expose
+        private String name;
+        @SerializedName("origin_country")
+        @Expose
+        private String originCountry;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public String getLogoPath() {
+            return logoPath;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getOriginCountry() {
+            return originCountry;
+        }
+    }
 }
